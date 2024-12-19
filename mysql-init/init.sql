03ac735079aecc07235eb6ca81dde6cc4a1652e0
Oskar Kobza <oskarkobza@gmail.com>
2024-12-19 13:22:45 +0100

    Init push, updated resume and storytime for new deployment system

---
+++ mysql-init/init.sql
@@ -0,0 +1,14 @@
+-- Tworzenie dodatkowych baz danych
+CREATE DATABASE IF NOT EXISTS resume;
+CREATE DATABASE IF NOT EXISTS storytime;
+
+-- Tworzenie pierwszego użytkownika
+CREATE USER 'user_resume'@'%' IDENTIFIED BY 'secret';
+GRANT ALL PRIVILEGES ON resume.* TO 'user_resume'@'%';
+
+-- Tworzenie drugiego użytkownika
+CREATE USER 'user_storytime'@'%' IDENTIFIED BY 'secret';
+GRANT ALL PRIVILEGES ON storytime.* TO 'user_storytime'@'%';
+
+-- Opcjonalnie: odświeżanie uprawnień
+FLUSH PRIVILEGES;
