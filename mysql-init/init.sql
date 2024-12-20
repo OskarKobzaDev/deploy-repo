6d6e2b8ebd50cfa9ccf26a2361ae6330e7c92710
root <root@production-oskarkobza.pl>
2024-12-20 08:36:30 +0000

    Working prod

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -3,11 +3,11 @@ CREATE DATABASE IF NOT EXISTS resume;
 CREATE DATABASE IF NOT EXISTS storytime;
 
 -- Tworzenie pierwszego użytkownika
-CREATE USER 'user_resume'@'%' IDENTIFIED BY 'secret';
+CREATE USER 'user_resume'@'%' IDENTIFIED BY 'Koczkodan1972!*';
 GRANT ALL PRIVILEGES ON resume.* TO 'user_resume'@'%';
 
 -- Tworzenie drugiego użytkownika
-CREATE USER 'user_storytime'@'%' IDENTIFIED BY 'secret';
+CREATE USER 'user_storytime'@'%' IDENTIFIED BY 'Koczkodan1972!*';
 GRANT ALL PRIVILEGES ON storytime.* TO 'user_storytime'@'%';
 
 -- Opcjonalnie: odświeżanie uprawnień
