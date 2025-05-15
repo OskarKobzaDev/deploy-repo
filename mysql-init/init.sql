f127590befa551926365802deb64fce7d8f17e39
Oskar Kobza <oskarkobza@gmail.com>
2025-05-15 19:28:04 +0200

    Pipeline testing 2

--- mysql-init/init.sql
+++ mysql-init/init.sql
@@ -1,14 +1,14 @@
--- Tworzenie dodatkowych baz danych
+cat <<EOF > init.sql
 CREATE DATABASE IF NOT EXISTS resume;
 CREATE DATABASE IF NOT EXISTS storytime;
 
--- Tworzenie pierwszego użytkownika
-CREATE USER 'user_resume'@'%' IDENTIFIED BY 'Koczkodan1972!*';
+CREATE USER 'user_resume'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON resume.* TO 'user_resume'@'%';
 
--- Tworzenie drugiego użytkownika
-CREATE USER 'user_storytime'@'%' IDENTIFIED BY 'Koczkodan1972!*';
+CREATE USER 'user_storytime'@'%' IDENTIFIED BY '${DB_PASSWORD}';
 GRANT ALL PRIVILEGES ON storytime.* TO 'user_storytime'@'%';
 
--- Opcjonalnie: odświeżanie uprawnień
 FLUSH PRIVILEGES;
+EOF
+
+mysql -u root -p"$MYSQL_ROOT_PASSWORD" < init.sql
