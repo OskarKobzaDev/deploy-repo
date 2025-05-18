5b637902cd3b03a738273faff9bbf28edebecef5
Oskar Kobza <oskarkobza@gmail.com>
2025-05-18 15:09:16 +0200

    test new 3

--- .github/workflows/deploy_scripts/generate_env.sh
+++ .github/workflows/deploy_scripts/generate_env.sh
@@ -3,7 +3,7 @@ set -e
 
 echo "Podmieniam zmienne środowiskowe..."
 
-sed -i "s|DB_PASSWORD=.*|DB_PASSWORD=${MYSQL_PASSWORD}|" .env
+sed -i "s|DB_PASSWORD=.*|MYSQL_PASSWORD=${MYSQL_PASSWORD}|" .env
 sed -i "s|REDIS_PASSWORD=.*|REDIS_PASSWORD=${REDIS_PASSWORD}|" .env
 sed -i "s|MAIL_PASSWORD=.*|MAIL_PASSWORD=${MAIL_PASSWORD}|" .env
 
